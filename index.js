8f0c3cb1e333fee6562b5d1893242df4bc8742b5
Lundy Orlando <simplyeazy@gmail.com>
2018-09-09 18:06:55 +0700

    Release 0 Check Vowel

--- index.js
+++ index.js
@@ -14,7 +14,38 @@ function generateArray(rowlen, colLen) {
         }
         row.push(col);
     }
+    console.log(row);
     return row;
 }
 
-console.log(generateArray(5, 4))
+function checkElementIsVowel(char) {
+    const vowels = "AIUEO";
+    for (let i = 0; i < vowels.length; i++) {
+        if (vowels[i] === char) {
+            return true;
+        }
+    }
+}
+
+function checkBlockForVowels(block) {
+    let vowelBlock = [];
+    let count = 0;
+    for (let i = 0; i < block.length - 1; i++) {
+        for (let j = 0; j < block[i].length; j++) {
+            if (checkElementIsVowel(block[i][j]) === true
+                && checkElementIsVowel(block[i][j + 1]) === true
+                && checkElementIsVowel(block[i + 1][j]) === true
+                && checkElementIsVowel(block[i + 1][j + 1]) === true) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+//generateArray(5, 4)
+console.log(checkBlockForVowels([
+    ['A', 'X', 'C', 'Y'],
+    ['E', 'O', 'O', 'S'],
+    ['I', 'U', 'I', 'N'],
+    ['M', 'Y', 'O', 'E'],
+    ['P', 'D', 'A', 'I']]));
